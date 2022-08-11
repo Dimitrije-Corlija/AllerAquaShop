49c7455d2a57cef6bbf14a07e612c5395ac9b1a1
Dimitrije-Corlija <56803786+Dimitrije-Corlija@users.noreply.github.com>
2022-08-11 13:52:40 +0200

    Created Postman request

---
+++ frontend/src/products.js
@@ -0,0 +1,108 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Aller Aqua Classic 2mm',
+    image: '/images/AllerAquaClassic2mm.jpg',
+    description:
+      'Aller Aqua Classic 2mm is the brilliant feed',
+    brand: 'Aller Aqua Balkan',
+    category: 'Feed',
+    price: 800,
+    countInStock: 10,
+    rating: 8,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Aller Aqua Classic 3mm',
+    image: '/images/AllerAquaClassic3mm.jpg',
+    description:
+      'Aller Aqua Classic 3mm is the brilliant feed',
+    brand: 'Aller Aqua Balkan',
+    category: 'Feed',
+    price: 850,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Aller Aqua Classic 4mm',
+    image: '/images/AllerAquaClassic4mm.jpg',
+    description:
+      'Aller Aqua Classic 4mm is the brilliant feed',
+    brand: 'Aller Aqua Balkan',
+    category: 'Feed',
+    price: 870,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Aller Aqua Master 4mm',
+    image: '/images/AllerAquaMaster4mm.jpg',
+    description:
+      'Aller Aqua Master 4mm is the brilliant feed',
+    brand: 'Aller Aqua Balkan',
+    category: 'Feed',
+    price: 950,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Aller Aqua Master 6mm',
+    image: '/images/AllerAquaMaster6mm.jpg',
+    description:
+      'Aller Aqua Master 6mm is the brilliant feed',
+    brand: 'Aller Aqua Balkan',
+    category: 'Feed',
+    price: 1000,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Aller Aqua Master 8mm',
+    image: '/images/AllerAquaMaster8mm.jpg',
+    description:
+      'Aller Aqua Master 8mm is the brilliant feed',
+    brand: 'Aller Aqua Balkan',
+    category: 'Feed',
+    price: 1000,
+    countInStock: 7,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '7',
+    name: 'Aller Aqua Top 3mm',
+    image: '/images/AllerAquaTop3mm.jpg',
+    description:
+      'Aller Aqua Top 3mm is the brilliant feed',
+    brand: 'Aller Aqua Balkan',
+    category: 'Feed',
+    price: 1200,
+    countInStock: 7,
+    rating: 4,
+    numReviews: 12,
+  },
+  {
+    _id: '8',
+    name: 'Aller Aqua Top 6mm',
+    image: '/images/AllerAquaTop6mm.jpg',
+    description:
+      'Aller Aqua Top 6mm is the brilliant feed',
+    brand: 'Aller Aqua Balkan',
+    category: 'Feed',
+    price: 1300,
+    countInStock: 7,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+export default products
