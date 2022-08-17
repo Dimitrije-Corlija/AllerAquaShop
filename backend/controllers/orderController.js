9d5c4b86eeb6537e25fc60dd5b208cf3bd42aecf
Dimitrije-Corlija <56803786+Dimitrije-Corlija@users.noreply.github.com>
2022-08-17 15:10:51 +0200

    Popravka uspesna

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -57,26 +57,26 @@ const getOrderById = asyncHandler(async (req,res) => {
 //@desc Update order to paid
 //@route GET /api/orders/:id/pay
 //@access Private
-const updateOrderToPaid = asyncHandler(async (req,res) => {
-    const order = await Order.findById(req.params.id)
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+    const order = await Order.findById(req.params.id);
 
-    if(order) {
-     order.isPaid = true
-     order.paidAd = Data.now()
-     order.paymentResult = {
-        id:req.body.id,
+    if (order) {
+      order.isPaid = true;
+      order.paidAt = Date.now();
+      order.paymentResult = {
+        id: req.body.id,
         status: req.body.status,
         update_time: req.body.update_time,
-        email_address: req.body.payer.email_address
-     }
-     const updateOrder = await order.save()
-     res.json(updateOrder)
-    } else {
-     res.status(404)
-     throw new Error('Order not found')
-    }
+        email_address: req.body.payer.email_address,
+      };
 
- })
+      const updatedOrder = await order.save();
+      res.json(updatedOrder);
+    } else {
+      res.status(404);
+      throw new Error("Order not found! Bad request.");
+    }
+  });
 
  //@desc Update order to delivered
 //@route GET /api/orders/:id/deliver
@@ -86,15 +86,15 @@ const updateOrderToDelivered = asyncHandler(async (req,res) => {
 
     if(order) {
      order.isDelivered = true
-     order.deliveredAt = Data.now()
-     
-     
-     const updateOrder = await order.save()
-     res.json(updateOrder)
+     order.deliveredAt = Date.now()
     } else {
      res.status(404)
      throw new Error('Order not found')
     }
+
+    const updateOrder = await order.save()
+    console.log(`Updated order: ${JSON.stringify(updateOrder)}`)
+    res.json(updateOrder)
 
  })
 
